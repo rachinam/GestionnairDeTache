a9ebb5e73b42fddaf06ccc1063bc94cbdd97bff2
rachinam <aryan.kona@live.fr>
2017-12-26 23:17:19 +0100

    tache

---
+++ src/g_de_tache/G_de_tache.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g_de_tache;
+
+/**
+ *
+ * @author aryan
+ */
+public class G_de_tache {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
